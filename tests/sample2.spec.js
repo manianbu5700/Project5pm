f7d871592fc7e17c93a8f3ca1a9d3c06ed7ea830
manianbu5700 <manianbu5700@gmail.com>
2026-03-19 14:16:09 +0530

    file modfied

--- tests/sample2.spec.js
+++ tests/sample2.spec.js
@@ -3,3 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Visit flipkart page', async ({ page }) => {
     await page.goto('https://www.amazon.com/')
 })
+test('Suggested application', async ({ page }) => {
+    await page.goto('https://www.mynthra.com/')
+})
